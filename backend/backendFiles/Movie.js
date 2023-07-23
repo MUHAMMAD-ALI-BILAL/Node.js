3f2ac4a297eb849086b23148ae11b1041d5fbb76
ALI BILAL <74378548+MUHAMMAD-ALI-BILAL@users.noreply.github.com>
2023-07-23 18:50:34 +0500

    Update Movie.js

--- backend/backendFiles/Movie.js
+++ backend/backendFiles/Movie.js
@@ -102,5 +102,5 @@ app.post('/api/videoPlay', (req, res) => {
 
 // Start the server
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port`);
+  console.log(`Server is running on port ${PORT}`);
 });
